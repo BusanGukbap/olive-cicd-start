e9aa45cc9128ffa89a2d268297948888f36c4305
BusanGukbap <dong2857210@gmail.com>
2025-01-14 10:53:59 +0900

    add workflow

--- src/main/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplication.java
+++ src/main/java/me/dangtong/springbootdeveloper/SpringbootdeveloperApplication.java
@@ -1,6 +1,6 @@
 package me.dangtong.springbootdeveloper;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
